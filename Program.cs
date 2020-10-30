ad0b71dbf0fe3fbe91df772033ce7d5df63fed28
miglio000 <gabrielemiglio23@gmail.com>
2020-10-30 10:03:37 +0100

    Decimal to binary.

--- Program.cs
+++ Program.cs
@@ -6,17 +6,26 @@ namespace FunctionDecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert a decimal number:");
-            int Ndecimal = Convert.ToInt32(Console.ReadLine());
+
+            /*int Ndecimal = Convert.ToInt32(Console.ReadLine());
             int remainder;
             string result = string.Empty;
+            string input, output;
             while(Ndecimal > 0)
             {
                 remainder = Ndecimal % 2;
                 Ndecimal /= 2;
-                result = remainder.ToString() + result;
+                result = remainder.ToString() + result; 
+                 input = "8";
+                 output = Convert.ToInt32(input, 2).ToString();
             }
-            Console.WriteLine($"The number in binary is {result}.");
+            Console.WriteLine($"The number in binary is {output}."); */
+            Console.WriteLine("Insert a decimal number:");
+            string number = Convert.ToString(Console.ReadLine());
+            int FromBase = 10;
+            int ToBase = 2;
+            string result = Convert.ToString(Convert.ToInt32(number, FromBase), ToBase);
+            Console.WriteLine($"Il numero binario è {result}.");
         }
     }
 }
