29062d341fbee6279a6c9d52880f92c187815f55
miglio000 <gabrielemiglio23@gmail.com>
2020-10-30 09:45:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FunctionDecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a decimal number:");
+            int Ndecimal = Convert.ToInt32(Console.ReadLine());
+            int remainder;
+            string result = string.Empty;
+            while(Ndecimal > 0)
+            {
+                remainder = Ndecimal % 2;
+                Ndecimal /= 2;
+                result = remainder.ToString() + result;
+            }
+            Console.WriteLine($"The number in binary is {result}.");
+        }
+    }
+}
